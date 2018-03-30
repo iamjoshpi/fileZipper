5335949b6462c96d7c6abbfb457d66539324a378
Joshua Whitney <joshwhitney.designer@gmail.com>
2018-03-29 23:38:05 -0400

    Create zipClass.php

---
+++ zipClass.php
@@ -0,0 +1,19 @@
+<?php
+function zipmaker($zipname){
+$zip = new ZipArchive();
+$filename = "$zipname.zip";
+
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+    exit("cannot open <$filename>\n");
+}
+
+$zip->addFromString("testfilephp.txt" . time(), "#1 This is a test string added as testfilephp.txt.\n");
+$zip->addFromString("testfilephp2.txt" . time(), "#2 This is a test string added as testfilephp2.txt.\n");
+$zip->addFile($thisdir . "/too.php","/testfromfile.php");
+echo "numfiles: " . $zip->numFiles . "\n";
+echo "status:" . $zip->status . "\n";
+$zip->close();
+}
+zipmaker("ccfiles");
+?>
+<a href="C:/Users/ciojmw/Desktop/reports/mar-report.pdf">link</a>
